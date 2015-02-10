14417f64ba2c1b7b675a63e18ca3f830aeecf276
Larry Lynn <llynn@takata.com>
2015-02-10 15:08:17 -0500

    Alcolator progress

---
+++ WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Larry Lynn on 12/16/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
